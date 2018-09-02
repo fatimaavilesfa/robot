cecc7b094344ddab80e8c14c62cfbfbe0e3ad31e
fatimaavilesfa <fatimaaviles1612@gmail.com>
2018-09-01 21:08:17 -0500

    first Exercise Hello word

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import App from './App';
+import Hello from './Hello.js';
+import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
+
+ReactDOM.render(<Hello greeting={'Hello ' + 'React Ninja'}/>, document.getElementById('root'));
+registerServiceWorker();
